efd63c48156ea33ab0e16e76c595aea2209050d8
Bogdan Farca <bogdan@farca.ro>
2020-08-26 13:23:47 +0300

    Initial Commit

---
+++ Shared/ATP_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATP_ClientApp.swift
+//  Shared
+//
+//  Created by Bogdan Farca on 26/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct ATP_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
